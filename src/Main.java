b1c67d60e80503c02c59177290720a5585487d97
Enokd <edragon851@students.mchenry.edu>
2024-01-17 14:13:39 -0600

    trying to commit Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Enok.");
+        System.out.println("I've been coding for two years.");
+        System.out.println("I've never worked with mobile apps.");
+        System.out.println("But I am excited to learn.");
+    }
+}
